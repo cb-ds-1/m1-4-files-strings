95ad568e45afbe7e2330cad2ade68735c6d55f17
Martin Dionne <martindionne@gmail.com>
2020-10-24 10:21:18 -0400

    minor changes

--- hamlet.py
+++ hamlet.py
@@ -1,9 +1,9 @@
 
 import os
 
-def mentionned():
-    f = open('data/hamlet.txt', 'r')
-    txt = f.read()
-    f.close()
-    answer = "Hamlet is mentionned " + str(txt.count("Hamlet")) + " times"
-    return answer
+def mentionned(word):
+    
+    with open("data/hamlet.txt", "r") as f:
+        txt = f.read().lower()
+        word_count = txt.count(word.lower())
+        return word_count
