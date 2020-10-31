8a1e4fe81fe1627b3b1982d2431a892d6a1fc3ce
Sadnan Saquif <ssaquif@gmail.com>
2020-10-31 01:42:43 -0400

    completed all

--- hamlet_count.py
+++ hamlet_count.py
@@ -1,6 +1,6 @@
 
 def hamlets_in_file(file):
-    with open(file, 'r') as buffer:
+    with open(file, 'r', encoding='utf-8-sig') as buffer:
         data = buffer.read()
 
     return data.lower().count("hamlet")
